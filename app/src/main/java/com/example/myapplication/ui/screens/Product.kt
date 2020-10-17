8e8a21e28e492f7ccf51e88e4e01d17292a51089
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2020-10-18 00:21:30 +0200

    [UPDATE] display profile

--- app/src/main/java/com/example/myapplication/ui/screens/Product.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/Product.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.ui.screens
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
 import com.example.myapplication.api.dto.TaskDto
@@ -10,22 +11,17 @@ import com.google.gson.Gson
 
 class Product : AppCompatActivity() {
     lateinit var task: TaskDto
+    lateinit var title: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product)
 
         val intent = intent
-        intent.getStringExtra("product").let { taskFromListAsJson -> task = Gson().fromJson(
-            taskFromListAsJson,
-            TaskDto::class.java
-        )
-
-
-
+        intent.getStringExtra("product").let { taskFromListAsJson ->
+            task = Gson().fromJson(taskFromListAsJson, TaskDto::class.java)
+            title = findViewById(R.id.product_profile_title)
+            title.text = task.title
         }
-
-        Log.d("INTENT PRODUCT : ", task.title)
-
     }
 }
